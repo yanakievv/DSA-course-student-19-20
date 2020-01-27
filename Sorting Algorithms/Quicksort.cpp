97a92d1859bab64d608950f2cd75ff4d757a49cc
yanakievv <48991798+yanakievv@users.noreply.github.com>
2020-01-27 19:57:56 +0200

    Create Quicksort.cpp

---
+++ Sorting Algorithms/Quicksort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int partition(int* arr, int l, int r)
+{
+	int pivot = arr[r];
+	int pivotindex = l;
+	for (int i = l; i <= r; i++)
+	{
+		if (arr[i] < pivot)
+		{
+			swap(arr[i], arr[pivotindex]);
+			pivotindex++;
+		}
+	}
+	swap(arr[r], arr[pivotindex]);
+	return pivotindex;
+}
+
+void _quicksort(int* arr, int l, int r)
+{
+	if (l < r)
+	{
+		int pivot = partition(arr, l, r);
+		_quicksort(arr, l, pivot - 1);
+		_quicksort(arr, pivot + 1, r);
+	}
+}
+
+void quicksort(int* arr, int lenght)
+{
+	_quicksort(arr, 0, lenght - 1);
+}
+
+int main() {
+
+	int arr[] = { 2, 5, 3, 6, 1, 4 };
+	int * arrStart = &arr[0];
+
+	quicksort(arrStart, 6);
+
+	for (int i = 0; i < 6; i++)
+	{
+
+		std::cout << arr[i] << ", ";
+
+	}
+	std::cout << "\n";
+
+}
